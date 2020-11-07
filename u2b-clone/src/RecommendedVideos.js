7c531c063a5e83c9272ae35a437582f9224e16db
Jerome Lam <j2lam@ucsd.edu>
2020-11-06 21:46:00 -0800

    Finished the Recommended Part!

--- u2b-clone/src/RecommendedVideos.js
+++ u2b-clone/src/RecommendedVideos.js
@@ -1,10 +1,94 @@
 import React from 'react';
+import VideoCard from './VideoCard.js';
 import './RecommendedVideos.css';
+
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h2>This is Recommended Videos</h2>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos_videos">
+        <VideoCard
+          title="My Hero Academia"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.pinimg.com/originals/d7/fb/37/d7fb37c2cd9cf33d39439af73463f695.png"
+          channel="Weebs America"
+          image="https://img1.hulu.com/user/v3/artwork/36e318dc-3daf-47fb-8219-9e3cb5cd28f2?base_image_bucket_name=image_manager&base_image=212cfe67-0a22-4097-9569-f179ee209d8a&region=US&format=jpeg&size=952x536"
+        />
+        <VideoCard
+          title="Big Hero 6 | Bot Fighting"
+          views="2.7M Views"
+          timestamp="2 days ago"
+          channelImage="https://64.media.tumblr.com/23dd52cbf1e51b2f25815600139fd8c5/tumblr_ptl91riFUm1su7lsa_540.png"
+          channel="Yessir89"
+          image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/16/10/Big-Hero-6-Movie-2014.jpg"
+        />
+        <VideoCard
+          title="Trading 101"
+          views="1M Views"
+          timestamp="1 month ago"
+          channelImage="https://vignette.wikia.nocookie.net/moviedatabase/images/8/8c/Tony_Stark.jpg/revision/latest/scale-to-width-down/340?cb=20150430161420"
+          channel="EatSleepProfit189"
+          image="https://i.ytimg.com/vi/jRNy7rTJK-A/hqdefault.jpg"
+        />
+        <VideoCard
+          title="My Hero Academia"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.pinimg.com/originals/d7/fb/37/d7fb37c2cd9cf33d39439af73463f695.png"
+          channel="Weebs America"
+          image="https://img1.hulu.com/user/v3/artwork/36e318dc-3daf-47fb-8219-9e3cb5cd28f2?base_image_bucket_name=image_manager&base_image=212cfe67-0a22-4097-9569-f179ee209d8a&region=US&format=jpeg&size=952x536"
+        />
+        <VideoCard
+          title="Big Hero 6 | Bot Fighting"
+          views="2.7M Views"
+          timestamp="2 days ago"
+          channelImage="https://64.media.tumblr.com/23dd52cbf1e51b2f25815600139fd8c5/tumblr_ptl91riFUm1su7lsa_540.png"
+          channel="Yessir89"
+          image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/16/10/Big-Hero-6-Movie-2014.jpg"
+        />
+        <VideoCard
+          title="Trading 101"
+          views="1M Views"
+          timestamp="1 month ago"
+          channelImage="https://vignette.wikia.nocookie.net/moviedatabase/images/8/8c/Tony_Stark.jpg/revision/latest/scale-to-width-down/340?cb=20150430161420"
+          channel="EatSleepProfit189"
+          image="https://i.ytimg.com/vi/jRNy7rTJK-A/hqdefault.jpg"
+        />
+        <VideoCard
+          title="My Hero Academia"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.pinimg.com/originals/d7/fb/37/d7fb37c2cd9cf33d39439af73463f695.png"
+          channel="Weebs America"
+          image="https://img1.hulu.com/user/v3/artwork/36e318dc-3daf-47fb-8219-9e3cb5cd28f2?base_image_bucket_name=image_manager&base_image=212cfe67-0a22-4097-9569-f179ee209d8a&region=US&format=jpeg&size=952x536"
+        />
+        <VideoCard
+          title="Big Hero 6 | Bot Fighting"
+          views="2.7M Views"
+          timestamp="2 days ago"
+          channelImage="https://64.media.tumblr.com/23dd52cbf1e51b2f25815600139fd8c5/tumblr_ptl91riFUm1su7lsa_540.png"
+          channel="Yessir89"
+          image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/16/10/Big-Hero-6-Movie-2014.jpg"
+        />
+        <VideoCard
+          title="Trading 101"
+          views="1M Views"
+          timestamp="1 month ago"
+          channelImage="https://vignette.wikia.nocookie.net/moviedatabase/images/8/8c/Tony_Stark.jpg/revision/latest/scale-to-width-down/340?cb=20150430161420"
+          channel="EatSleepProfit189"
+          image="https://i.ytimg.com/vi/jRNy7rTJK-A/hqdefault.jpg"
+        />
+        <VideoCard
+          title="My Hero Academia"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://i.pinimg.com/originals/d7/fb/37/d7fb37c2cd9cf33d39439af73463f695.png"
+          channel="Weebs America"
+          image="https://img1.hulu.com/user/v3/artwork/36e318dc-3daf-47fb-8219-9e3cb5cd28f2?base_image_bucket_name=image_manager&base_image=212cfe67-0a22-4097-9569-f179ee209d8a&region=US&format=jpeg&size=952x536"
+        />
+      </div>
     </div>
   );
 }
